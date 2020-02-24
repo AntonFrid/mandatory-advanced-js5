054a5794801188a6ead16db35a180e14e13e737f
Linus Viberg <linuswiberg96@gmail.com>
2020-02-24 14:46:55 +0100

    added display of username and logout button

--- src/components/Menupanel.js
+++ src/components/Menupanel.js
@@ -3,10 +3,25 @@ import { Dropbox } from 'dropbox';
 import { Router, Link } from 'react-router-dom';
 
 class Menupanel extends React.Component {
+  constructor(props){
+    super(props)
+
+
+  }
+
+  logOut(){
+    console.log("logged out");
+    
+  }
+
   render() {
     return (
       <div className='menupanel'>
-        <img src="/media/icon_text.png" alt="logo" id="logo"/>
+        <img src="/media/icon_text.png" alt="logo" className="logo"/>
+        <div className="menupanelBottom">
+          <h3 className="mpUserName">USER</h3>
+          <button className="logout-btn" onClick={this.logOut}>Logout</button>
+        </div>
       </div>
     );
   }
