c6203891cd182de5e560a8bf93e435e6172300ff
Ted Larsson <larssonted86@gmail.com>
2020-02-24 12:39:23 +0100

    added redirect to login and added Auth Component and route to auth

---
+++ src/components/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Auth extends Component {
+    render() {
+        return (
+            <div>
+                <p>auth site</p>
+            </div>
+        )
+    }
+}
+
+export default Auth
