3dce7060a17ea9d972a84a372913caf9855f7006
Ted Larsson <larssonted86@gmail.com>
2020-02-24 13:08:25 +0100

    added function to get accessToken to Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
 
 export class Auth extends Component {
+
+   getToken = () => {
+    const string = window.location.hash;
+        let array = string.split('=');
+        let newString = array[1];
+        let newArray = newString.split('&')
+        let token = newArray[0]
+        
+        console.log(array)
+        console.log(newArray)
+        console.log(token)
+   }
+
+   componentDidMount(){
+       this.getToken()
+   }
+
     render() {
+        
         return (
             <div>
                 <p>auth site</p>
